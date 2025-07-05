7c81a368f3156d5ca21be72f81a588d841d1cd8f
cherryorg33 <talariramcharan33@gmil.com>
2025-07-05 08:33:52 +0530

    added feat:health check and head and tails pages

---
+++ src/test/java/in/devmtr/javamaven/MoviesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.devmtr.javamaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
